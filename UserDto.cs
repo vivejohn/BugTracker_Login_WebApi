899ad7d4fc6fa05d1cc68c272b5e5b1d452a7a7b
John Mejicanos <79382883+vivejohn@users.noreply.github.com>
2022-04-18 15:44:57 -0700

    Add project files.

---
+++ UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BugTracker_Login_WebApi
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
